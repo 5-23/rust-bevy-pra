032170cbd895a786dd48cc9f89415593b728dd36
5-23 <yhanbyeol6@gmail.com>
2023-10-12 03:15:55 +0900

    feat: make sprite

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,23 @@
+use std::io::Write;
+
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, start)
-        .add_plugins(TimerPlugin)
+        .add_systems(Startup, setup)
         .run();
 }
 
-#[derive(Debug, Component)]
-struct Entity {
-    id: usize,
-    name: String
-}
 
-impl Entity {
-    fn new(id: usize, name: String) -> Self {
-        Self { id, name }
-    }
-}
-
-fn start(mut cmd: Commands) {
-    cmd.spawn(Entity::new(1, "5-23".to_string()));
-    cmd.spawn(Entity::new(2, "5-24".to_string()));
-}
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(
-    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Entity>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for e in &query {
-            println!("{e:?}");
-        }
-    }
-}
-
-struct TimerPlugin;
-
-impl Plugin for TimerPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Update, greet_people);
-    }
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(100.0, 100.0)),
+            color: Color::rgb(0.0, 0.0, 0.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
